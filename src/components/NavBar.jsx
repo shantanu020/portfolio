56b9ac1ae754b6e26b90a4288491e23e10a5f1c5
Shantanu yadav <shantanuytech@gmail.com>
2025-07-17 01:47:32 +0530

    changed prject

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> PedroTech </span>{" "}
+            <span className="text-glow text-foreground"> Shantanu Yadav </span>{" "}
             Portfolio
           </span>
         </a>
